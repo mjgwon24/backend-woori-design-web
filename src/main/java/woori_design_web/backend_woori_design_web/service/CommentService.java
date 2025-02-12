6d501f45f184687b786efa54dd6effe593712cc9
mjgwon24 <alswlchlrh8@naver.com>
2025-02-12 11:37:51 +0900

    feat: User, Comment, UserLike, Auth service interface, impl 틀 구현

---
+++ src/main/java/woori_design_web/backend_woori_design_web/service/CommentService.java
@@ -0,0 +1,15 @@
+package woori_design_web.backend_woori_design_web.service;
+
+/**
+ * 댓글 서비스
+ * @author 조윤주
+ * @since 2025.02.12
+ */
+public interface CommentService {
+    // 댓글 등록
+
+    // 댓글 개수 조회
+
+    // 댓글 조회
+
+}
