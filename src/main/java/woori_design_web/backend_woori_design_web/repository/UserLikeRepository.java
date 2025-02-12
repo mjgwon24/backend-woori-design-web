728519d5abe35409fb23fde8b3916108e949855e
seocylucky <seocylucky@github.com>
2025-02-12 16:36:19 +0900

    feat: postId 존재 여부 및 좋아요 개수를 조회하는 쿼리 추가

--- src/main/java/woori_design_web/backend_woori_design_web/repository/UserLikeRepository.java
+++ src/main/java/woori_design_web/backend_woori_design_web/repository/UserLikeRepository.java
@@ -1,6 +1,8 @@
 package woori_design_web.backend_woori_design_web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import woori_design_web.backend_woori_design_web.entity.UserLike;
 
@@ -11,4 +13,9 @@ import java.util.Optional;
 public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
     List<UserLike> findByPostId(Long postId);
     Optional<UserLike> findByUserIdAndPostId(Long userId, Long postId);
+
+    @Query("SELECT COUNT(ul) FROM UserLike ul WHERE ul.postId = :postId")
+    Long countLikesByPostId(@Param("postId") Long postId);
+
+    boolean existsByPostId(Long postId);
 }
